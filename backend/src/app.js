ce0e9893448825809c664e1348e47a843a50f58e
Jeff <mjfbarrientos@tip.edu.ph>
2025-10-27 17:58:32 +0800

    Commented frontend dist in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -45,13 +45,13 @@ app.use("/api/specialties-and-services", specialtyAndServiceRoutes);
 app.use("/api/users", userRoutes);
 
 // ✅ Serve frontend only in production
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    // if (process.env.NODE_ENV === "production") {
+    //     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
-}
+    //     app.get("*", (req, res) => {
+    //         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    //     });
+    // }
 
 // ✅ Export app & DB connection for tests
 export { app, connectDB };
